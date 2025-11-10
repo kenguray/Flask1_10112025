42fd66f1de79d0a1df8c3e21d8dc4dfa1f9f56a8
Alexey Kosenko <kenguray@gmail.com>
2025-11-10 20:30:59 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
+about_me = {
+    "name": "Алексей",
+    "surname": "Геннадьевич",
+    "lastname": "Косенко",
+    "email": "cuctemarma@vk.com"
+}
 
 @app.route("/")  # Это первый URL, который мы будем обрабатывать
 def hello_world():  # Это функция-обработчик будет вызвана при запросе этого URL.
     return "Hello, World!"
 
+@app.route("/about")
+def about():
+    return about_me
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
