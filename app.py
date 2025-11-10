f52712864d1f1e5614ce9425c0c3ab17644147cd
kenguray <kenguray@gmail.com>
2025-11-11 01:42:59 +0300

    Implement quotes endpoint in app.py
    Added a new endpoint to return programming quotes.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
+app.json.ensure_ascii = False
 
 about_me = {
     "name": "Алексей",
@@ -10,15 +10,42 @@ about_me = {
     "email": "cuctemarma@vk.com"
 }
 
-@app.route("/")  # Это первый URL, который мы будем обрабатывать
-def hello_world():  # Это функция-обработчик будет вызвана при запросе этого URL.
+quotes = [
+    {
+        "id": 3,
+        "author": "Rick Cook",
+        "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+    },
+    {
+        "id": 5,
+        "author": "Waldi Ravens",
+        "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+    },
+    {
+        "id": 6,
+        "author": "Mosher’s Law of Software Engineering",
+        "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+    },
+    {
+        "id": 8,
+        "author": "Yoggi Berra",
+        "text": "В теории, теория и практика неразделимы. На практике это не так."
+    }
+]
+# Нужно больше цитат? https://tproger.ru/devnull/programming-quotes/
+
+
+@app.route("/")
+def hello_world():
     return "Hello, World!"
 
 @app.route("/about")
 def about():
     return about_me
 
+@app.route("/quotes")
+def get_quotes():
+    return jsonify(quotes)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
